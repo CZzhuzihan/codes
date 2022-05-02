45de70f9d8df58c9fe75e629321e79d4793ce515
CZzhuzihan <103350801+CZzhuzihan@users.noreply.github.com>
2022-05-02 09:58:21 +0800

    Update ch0205_1789.cpp

--- openjudge/2.5基本算法之搜索/ch0205_1789.cpp
+++ openjudge/2.5基本算法之搜索/ch0205_1789.cpp
@@ -1,57 +1,67 @@
 #include<bits/stdc++.h>
 using namespace std;
-double a[10];
-bool b[20];
-int z=0;
-bool check(){
-	for(int i=0;i<4;i++){
-		if(a[i]!=0) return false;
-	}
-	return true;
-}
-void dfs(int x,double s){
-    if(z) return;
-	if(x==3){
-        if(abs(s-24)<=0.000001){
-            z=1;
-        }
-        return;
-    }
-    for(int i=0;i<4;i++){
-        if(b[i]) continue;
-        b[i]=1;
-        for(int j=0;j<4;j++){
-            if(b[j]) continue;
-            b[j]=1;
-            double y=a[j];
-            for(int k=0;k<4;k++){
-                double s;
-                if(k==0) s=a[i]+a[j];
-                if(k==1) s=a[i]-a[j];
-                if(k==2) s=a[i]*a[j];
-                if(k==3) s=a[i]/a[j];
-                a[j]=s;
-                b[j]=0;
-                dfs(x+1,s);
-                a[j]=y;
-            }
-            b[j]=0;
-        }
-        b[i]=0;
-    }
-}
+vector<int> a;
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	while(1){
-		for(int i=0;i<4;i++){
-			cin>>a[i];
-            b[i]=0;
-		}
-		if(check()) break;
-		z=0;
-		dfs(0,0);
-		cout<<(z==1?"YES":"NO")<<endl;
+        int x,y;
+        cin>>x>>y;
+        if(x==0&&y==0) break;
+        a.clear();
+        while(y!=0){
+            a.push_back(y%10);
+            y=y/10;
+        }
+        reverse(a.begin(),a.end());
+        int l=a.size()-1;
+        int ans=-1;
+        vector<vector<int>> z;
+        for(int i=0;i<1<<l;i++){
+	        int c=0;
+            vector<int> v;
+		    for(int j=0;j<l;j++){
+                if(i>>j&1){
+                    int b=0;
+                    for(int k=c;k<=j;k++){
+                    	b=b*10+a[k];
+                    }
+                    c=j+1;
+                    v.push_back(b);
+                }
+            }
+            int d=0;
+            for(int k=c;k<a.size();k++){
+                d=d*10+a[k];
+            }
+            v.push_back(d);
+            int s=0;
+            for(auto j:v){
+                s=s+j;
+            }
+            if(s>x) continue;
+            if(s>ans){
+                ans=s;
+                z.clear();
+                z.push_back(v);
+            }
+            else if(s==ans){
+                z.push_back(v);
+            }
+        }
+        if(z.size()==0){
+            cout<<"error";
+        }
+        else if(z.size()>1){
+            cout<<"rejected";
+        }
+        else{
+            cout<<ans;
+            for(auto i:z[0]){
+                cout<<' '<<i;
+            }
+        }
+        cout<<endl;
 	}
-    return 0;
+	return 0;
 }
