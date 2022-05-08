87a989aeb4a727a29a01f43bdda0b5a37f4b544b
CZzhuzihan <103350801+CZzhuzihan@users.noreply.github.com>
2022-05-08 22:02:34 +0800

    Create abc250_b.cpp

---
+++ atcoder/abc250_b.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,a,b;
+    cin>>n>>a>>b;
+    for(int i=0;i<n*a;i++){
+        for(int j=0;j<n*b;j++){
+            if((i/a+j/b)%2==0) cout<<'.';
+            else cout<<'#';
+        }
+        cout<<endl;
+    }
+    return 0;
+}
