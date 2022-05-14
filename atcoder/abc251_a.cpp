1d8e0adcd9ac7b34e332574e467fe08ce5b566b2
CZzhuzihan <103350801+CZzhuzihan@users.noreply.github.com>
2022-05-14 22:24:22 +0800

    Create abc251_a.cpp

---
+++ atcoder/abc251_a.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    string a;
+    cin>>a;
+    for(int i=0;i<(6/a.size());i++){
+        cout<<a;
+    }
+    return 0;
+}
