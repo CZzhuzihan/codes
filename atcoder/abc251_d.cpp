0126b1bb069b3602635b1886f3c996acd74705e7
CZzhuzihan <103350801+CZzhuzihan@users.noreply.github.com>
2022-05-14 22:26:46 +0800

    Create abc251_d.cpp

---
+++ atcoder/abc251_d.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int w;
+    cin>>w;
+    cout<<298<<endl;
+    for(int i=1;i<=99;i++){
+        cout<<i<<' ';
+    }
+    for(int i=100;i<=9900;i+=100){
+        cout<<i<<' ';
+    }
+    for(int i=10000;i<=1000000;i+=10000){
+        cout<<i<<' ';
+    }
+    return 0;
+}
